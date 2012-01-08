ffd2ae0d41e791446b76150f57010432dee78bd4
Krystian Wojtas <krystian.wojtas@gmail.com>
2012-01-08 16:49:20 +0100

    gitignore, sprawko->reports

--- aipo/5/Lab.rb
+++ aipo/5/Lab.rb
@@ -3,24 +3,24 @@ require 'FImg4R_KMM'
 class Lab
   
   def run(img_fn)
-    FImg4R.new("5/in/p2.bmp").kmm.write('5/out/kmm_p2_100.jpg')
-    FImg4R.new("5/in/p2.bmp").scienianie.write('5/out/sc_p2_100.jpg')
-=begin
+    #FImg4R.new("5/in/p2.bmp").kmm.write('5/out/kmm_p2_100.jpg')
+    #FImg4R.new("5/in/p2.bmp").scienianie.write('5/out/sc_p2_100.jpg')
+#=begin
     FImg4R.new("5/in/p1.bmp").kmm.write('5/out/kmm_p1_100.jpg')
     FImg4R.new("5/in/p2.bmp").kmm.write('5/out/kmm_p2_100.jpg')
     FImg4R.new("5/in/p3.bmp").kmm.write('5/out/kmm_p3_100.jpg')
     FImg4R.new("5/in/p4.bmp").kmm.write('5/out/kmm_p4_100.jpg')
     FImg4R.new("5/in/o1.bmp").kmm.write('5/out/kmm_o1_100.jpg')
     FImg4R.new("5/in/o2.bmp").kmm.write('5/out/kmm_o2_100.jpg')
-=end
+#=end
     
-=begin
+#=begin
     FImg4R.new("5/in/p1.bmp").scienianie.write('5/out/sc_p1_100.jpg')
     FImg4R.new("5/in/p1.bmp").scienianie.write('5/out/sc_p2_100.jpg')
     FImg4R.new("5/in/p3.bmp").scienianie.write('5/out/sc_p3_100.jpg')
     FImg4R.new("5/in/p4.bmp").scienianie.write('5/out/sc_p4_100.jpg')
     FImg4R.new("5/in/o1.bmp").scienianie.write('5/out/sc_o1_100.jpg')
     FImg4R.new("5/in/o2.bmp").scienianie.write('5/out/sc_o2_100.jpg')
-=end
+#=end
   end
 end
--- aipo/aop/FImg4R_write.rb
+++ aipo/aop/FImg4R_write.rb
@@ -2,6 +2,6 @@
 require 'aquarium'
 include Aquarium::Aspects
 
-Aspect.new :after, :calls_to => :write, :in_type => :FImg4R: do |jp, o, *args|
+Aspect.new :after, :calls_to => :write, :in_type => :FImg4R do |jp, o, *args|
   p "written #{args[0]}"
 end
